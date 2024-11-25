8a526cc7fabb30d1bc468eb3df638e05e7ae204b
Ankit Yadav <122344948+LiveWithCodeAnkit@users.noreply.github.com>
2024-11-25 15:08:31 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -67,7 +67,7 @@ export function Navbar() {
                   <NavigationMenuItem>
                     <NavigationMenuTrigger>Shop</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                      <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                      <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[400px]">
                         {categories.map((category) => (
                           <li key={category.title}>
                             <NavigationMenuLink asChild>
